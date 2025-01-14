73967ee0c1d549712a38bda336888cac0a921f27
Sudipto Ray <sudiptoray2003@gmail.com>
2025-01-14 12:57:31 +0530

    Implemented the board color selector

--- src/screens/BoardsScreen/CreateBoardModal.jsx
+++ src/screens/BoardsScreen/CreateBoardModal.jsx
@@ -1,24 +1,36 @@
 import React from 'react'
+import { useState } from 'react'
 import { Dialog, Stack, Typography, Box, IconButton, TextField, Button} from '@mui/material'
 import CloseIcon from '@mui/icons-material/Close'
 import ModalHeader from '../../components/layout/ModalHeader'
 import { colors } from '../../theme'
 const CreateBoardModal = () => {
+    const [name, setName]=useState('')
+    const [color, setColor]=useState(0)
+    console.log({name,color})
   return (
     <Dialog open fullWidth maxWidth='xs'>
         <Stack padding={2}>
             <ModalHeader title={"Create Board"}/>
             <Stack my={5} spacing={3}>
-                <TextField label="Board name"/>
+                <TextField value={name} onChange={(e)=> setName(e.target.value)} label="Board name"/>
                 <Stack spacing={1.5} direction={'row'}>
                     <Typography>Color: </Typography>
-                    {colors.map(clr => <Box
+                    {colors.map((clr,idx) => (
+                        <Box
+                        sx={{
+                            cursor: 'pointer',
+                            border: color === idx ? '3px solid #383838' : 'none',
+                            outline: `2px solid ${clr}`
+                        }}
+                        onClick={()=>setColor(idx)}
                         key={clr}
                         height={25}
                         width={25}
                         backgroundColor={clr}
                         borderRadius={"50%"}
-                    />)}
+                        />
+                    ))}
                 </Stack>
             </Stack>
             <Button size='large' variant='contained'>Create</Button>
